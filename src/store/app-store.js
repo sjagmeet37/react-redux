fb64a28106050cfdafe0e8d1f4a9fa99d2a0ad85
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-09-16 15:41:42 +0530

    adding slice

--- src/store/app-store.js
+++ src/store/app-store.js
@@ -1,39 +1,31 @@
-
-import {createStore} from 'redux';
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = {
-    counter : 0,
-    showCounter : true 
-}
+  counter: 0,
+  showCounter: true,
+};
 
-const reducer = (state = initialState, action) => {
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    incrementFlex(state, action) {
+      state.counter + action.amount;
+    },
+    toggleCounter(state, action) {
+      !state.showCounter;
+    },
+  },
+});
 
-    if(action.type === 'increment') {
-        return {
-            counter : state.counter + 1,
-            showCounter : state.showCounter
-        }
-    } else if (action.type === 'decrement') {
-        return {
-            counter : state.counter - 1,
-            showCounter : state.showCounter
-        }
-    } else if (action.type === 'increase') {
-        return {
-            counter : state.counter + action.amount,
-            showCounter : state.showCounter
-        }
-    }  else if (action.type === 'toggleCounter') {
-        return {
-            counter : state.counter,
-            showCounter : !state.showCounter
-        }
-    }
-
-    return state;
-
-}
-
-const store = createStore(reducer);
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer },
+});
 
 export default store;
