632442159ec5f55885ea6c8585db650358f7371a
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-09-16 12:16:32 +0530

    adding events

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from "./store/app-store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    {" "}
+  
     <App />
   </Provider>
 );
--- src/store/app-store.js
+++ src/store/app-store.js
@@ -1,33 +1,28 @@
 
-import createStore from 'redux';
+import {createStore} from 'redux';
 
 
 const reducer = (state = {
     counter : 0 
 }, action) => {
 
-    if(action === 'increment') {
+    if(action.type === 'increment') {
         return {
-            counter : counter + 1
+            counter : state.counter + 1
         }
-    } else if (action === 'decrement') {
+    } else if (action.type === 'decrement') {
         return {
-            counter : counter - 1
+            counter : state.counter - 1
         }
     }
+    console.log(action)
+    console.log(state)
 
+    console.log(state.counter)
     return state;
 
 }
 
 const store = createStore(reducer);
-
-const counterSubscriber = () => {
-    const latestState = store.getState();
-    console.log(latestState);
-}
-
-store.dispatch({type : 'increment'});
-
 
 export default store;
